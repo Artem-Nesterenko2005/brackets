70a4ea664e8c4615d980c3e0fe7db25d7d7929c4
Artem-Nesterenko2005 <144232708+Artem-Nesterenko2005@users.noreply.github.com>
2023-09-13 22:12:01 +0500

    Add files via upload

---
+++ brakets.c
@@ -0,0 +1,25 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+	char string[11] = {'(',')','(',')','(',')','(',')','(',')','\0'};
+	int counter = 0;
+	int symbolNumber = 0;
+	for (string[symbolNumber]; symbolNumber < 10; symbolNumber++)
+	{
+		if (string[symbolNumber] == '(')
+			counter += 1;
+		else
+			counter -= 1;
+		if (counter < 0)
+		{
+			printf("%d - The symbol is incorrect\n", symbolNumber);
+			printf("Error");
+			break;
+		}
+		else
+			printf("%d - The symbol is correct\n", symbolNumber);		
+	}
+}
+

0e25358a1e27f0ebd2e83cfeb51956de6dc41996
Artem-Nesterenko2005 <144232708+Artem-Nesterenko2005@users.noreply.github.com>
2023-09-17 20:30:22 +0500

    Add files via upload

---
+++ brackets.c
@@ -0,0 +1,21 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+
+#define limitation 10
+
+int main()
+{
+    char string[11];
+    printf("Enter 10 brackets for the bracket string ");
+    fgets(string, 11, stdin);
+    int counter = 0;
+    for (int symbolNumber = 0; symbolNumber < limitation; ++symbolNumber)
+    {
+        counter = string[symbolNumber] == '(' ? ++counter : --counter;
+        if (counter < 0)
+        {
+            break;
+        }
+    }
+    printf(counter == 0 ? "String is correct" : "String is not correct");
+}
